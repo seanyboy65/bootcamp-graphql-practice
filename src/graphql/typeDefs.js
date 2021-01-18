ce5f6ac3b755deb4603c41f910ea9550ceae2c02
seanyboy65 <spmccabe@college.harvard.edu>
2021-01-18 16:26:00 -0500

    grahql exercises from week 2

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -3,5 +3,67 @@ const { gql } = require('apollo-server-express')
 module.exports = gql`
   type Query {
     welcome: String!
+    books(author: Author!): [Book!]!
+    book(id: ID!): Book!
+    author(id: ID!: Author!
+    publisher: Publisher!
+
   }
+  type Mutation: {
+    addBook(input: addBookInput): Book!
+    addAuthor(input: addAuthorInput): Author!
+    deleteBook(id:ID!): Book!
+    deleteAuthor(id: ID!): Author!
+  }
+  type Address: {
+    id: ID!
+    street: String!
+    city: String!
+    state: String!
+    zip: String!
+  }
+  type Publisher:{
+    id: ID!
+    company: String!
+    phoneNumber: String!
+    numBooksPublished: Int!
+    address: Address!
+  }
+  type Author: {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+    numBooksPublished: Int!
+    address: Address!
+  }
+  type Book: {
+    id: ID!
+    title: String!
+    language: String!
+    numPages: Int!
+    datePublished: String!
+    bestseller: Boolean
+    author: Author!
+    publisher: Publisher!
+  }
+  input addBookInput: {
+    title: String!
+    language: String!
+    numPages: Int!
+    datePublished: String!
+    bestseller: Boolean
+    author: Author!
+    publisher: Publisher!
+  }
+  input addAuthorInput: {
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+    numBooksPublished: Int!
+    address: Address!
+  }
+
 `
